28ba7af23c9d7cce6091c227f5bfb69050369d1a
Artur Nozhenko <a.nozhenko@ukma.edu.ua>
2025-04-05 19:09:17 +0300

    start

---
+++ src/test/java/com/bd_ais/zlagoda/ZlagodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd_ais.zlagoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZlagodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
