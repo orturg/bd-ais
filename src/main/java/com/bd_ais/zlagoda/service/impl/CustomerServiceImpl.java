8e5b3ade1f12f3590aca9b61abf393ae4e695738
Dana <danamushchenko@gmail.com>
2025-04-15 13:58:02 +0300

    solve problem with CustomerServiceImpl

--- src/main/java/com/bd_ais/zlagoda/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/bd_ais/zlagoda/service/impl/CustomerServiceImpl.java
@@ -3,6 +3,7 @@ package com.bd_ais.zlagoda.service.impl;
 import com.bd_ais.zlagoda.dao.CustomerDao;
 import com.bd_ais.zlagoda.model.CustomerEntity;
 import com.bd_ais.zlagoda.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,34 +11,35 @@ import java.util.List;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
-    private final CustomerDao customerDao;
+    private final CustomerDao customerDaoImpl;
 
-    public CustomerServiceImpl(CustomerDao customerDao) {
-        this.customerDao = customerDao;
+    @Autowired
+    public CustomerServiceImpl(CustomerDao customerDaoImpl) {
+        this.customerDaoImpl = customerDaoImpl;
     }
 
     @Override
     public void addCustomer(CustomerEntity customer) {
-        customerDao.save(customer);
+        customerDaoImpl.create(customer);
     }
 
     @Override
     public CustomerEntity getCustomerById(Long id) {
-        return customerDao.findById(id);
+        return customerDaoImpl.getById(id).orElse(null);
     }
 
     @Override
     public List<CustomerEntity> getAllCustomers() {
-        return customerDao.findAll();
+        return customerDaoImpl.getAll();
     }
 
     @Override
     public void updateCustomer(CustomerEntity customer) {
-        customerDao.update(customer);
+        customerDaoImpl.update(customer);
     }
 
     @Override
     public void deleteCustomer(Long id) {
-        customerDao.delete(id);
+        customerDaoImpl.delete(id);
     }
 }
